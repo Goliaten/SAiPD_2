f1225aed61c5bd5de9c95ae2b5fdf286d58f151c
Goliaten <ognikat4@gmail.com>
2025-12-08 00:59:47 +0100

    dynamically import and include endpoints

--- src/app/api/v1/wars.py
+++ src/app/api/v1/wars.py
@@ -1,9 +1,27 @@
-from fastapi import APIRouter
+import glob
+from importlib import import_module
+import os
+from pathlib import Path
+from typing import List
+from fastapi import APIRouter, logger
 
-from src.app.api.v1.endpoints import (
-    UserManagement,
-)
+excluded_files: List[str] = ["__init__.py"]
+import_path = "src.app.api.v1.endpoints"
+path = import_path.replace(".", os.sep)
+
 
 router = APIRouter()
+for module in glob.glob(os.path.join(path, "*.py")):
+    file = os.path.split(module)[-1]
+    if file in excluded_files:
+        continue
 
-router.include_router(UserManagement.router)
+    try:
+        imported_module = import_module(f"{import_path}.{file[:-3]}")
+
+        router.include_router(imported_module.router)
+    except AttributeError as e:
+        logger.logger.error(f"Failed to import {file} module's router.")
+        import traceback
+
+        logger.logger.debug(traceback.format_exc())
