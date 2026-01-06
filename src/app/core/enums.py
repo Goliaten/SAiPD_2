67e584306a9a271d65f96c343d6cb979307518d9
Goliaten <ognikat4@gmail.com>
2026-01-06 23:21:55 +0100

    enum for attendance

--- src/app/core/enums.py
+++ src/app/core/enums.py
@@ -6,3 +6,11 @@ class history_status(Enum):
     finished = "finished"
     cancelled = "cancelled"
     not_started = "not_started"
+
+
+class attendance_status(Enum):
+    present = "present"
+    absent = "absent"
+    late = "late"
+    not_happened = "not_happened"
+    unknown = "unknown"
