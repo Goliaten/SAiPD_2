4d45f0e72e7a8b48c562a7a3a4c453269c884918
Goliaten <ognikat4@gmail.com>
2026-01-06 23:21:44 +0100

    empty routers for remaining functionalities

---
+++ src/app/api/v1/endpoints/MessageManagement.py
@@ -0,0 +1,10 @@
+import datetime
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List, Optional
+
+from src.app.schemas.Class import Class, InputClassData
+from src.app.database import crud
+from src.app.database.session import get_db
+
+router = APIRouter(prefix="/msg", tags=["msg"])
